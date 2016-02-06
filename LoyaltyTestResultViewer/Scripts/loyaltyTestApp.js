0c98a44301d7f866f52c4e1a650a86a52e94829f
sally <sally.duan@nordstrom.com>
2016-02-05 22:13:22 -0800

    http://localhost:18411/TestReport.html#/report/

---
+++ LoyaltyTestResultViewer/Scripts/loyaltyTestApp.js
@@ -0,0 +1,16 @@
+﻿angular.module("loyalty-report-app", ["ngRoute", "googlechart"]).config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/report/', {
+                templateUrl: 'partials/lrChart.html',
+                controller: 'lrChartController'
+            }).
+            otherwise({
+                redirectTo: '/generic'
+            });
+    }]).value('googleChartApiConfig', {
+        version: '1',
+        optionalSettings: {
+            packages: ['corechart', 'gauge']
+        }
+    });
